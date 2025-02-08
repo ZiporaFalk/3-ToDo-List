08fbf887c932ebe4396fe427d47fe2b64e5bcce9
ZiporaFalk <z0548498935@gmail.com>
2025-02-08 22:53:33 +0200

    REACT_APP_URL

--- ToDoListReact/src/config.js
+++ ToDoListReact/src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    API_BASE_URL: "http://localhost:5131/index.html"
-  };
-  
-  export default config;
+  // API_BASE_URL: process.REACT_APP_URL
+  apiUrl: process.env.REACT_APP_URL 
+  // API_BASE_URL: "http://localhost:5131/index.html"
+};
+
+export default config;
