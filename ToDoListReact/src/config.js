b21fa2eea362e0e45cf0b010646644e0208b1c18
ZiporaFalk <z0548498935@gmail.com>
2025-02-09 17:08:00 +0200

    check2

--- ToDoListReact/src/config.js
+++ ToDoListReact/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // apiUrl: process.env.REACT_APP_URL 
-  apiUrl: "http://localhost:5131"
+  apiUrl: process.env.REACT_APP_URL 
+  // apiUrl: "http://localhost:5131"
   // API_BASE_URL: "http://localhost:5131/index.html"
   // apiUrl: "http://localhost:5131/index.html"
 
